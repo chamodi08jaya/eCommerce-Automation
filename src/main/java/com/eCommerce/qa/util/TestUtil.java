906604621ff5bc89eb45d3a07bfe919772828b31
SachiniMadhumani <38650179+SachiniMadhumani@users.noreply.github.com>
2021-10-27 19:17:04 +0530

    E-Commerce Application

--- src/main/java/com/eCommerce/qa/util/TestUtil.java
+++ src/main/java/com/eCommerce/qa/util/TestUtil.java
@@ -16,53 +16,43 @@ public class TestUtil {
     public class WebEventListener extends TestBase implements WebDriverEventListener {
 
         public void afterAlertAccept(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("After Alert Accept "+arg0.toString() );
         }
 
         public void afterAlertDismiss(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("After Alert Dismiss "+ arg0);
         }
 
         public void afterChangeValueOf(WebElement arg0, WebDriver arg1,
                                        CharSequence[] arg2) {
-            // TODO Auto-generated method stub
             System.out.println("After value change of" +arg0);
         }
 
         public void afterClickOn(WebElement arg0, WebDriver arg1) {
-            // TODO Auto-generated method stub
             System.out.println("After clicked"+arg0);
         }
 
         public void afterFindBy(By arg0, WebElement arg1, WebDriver arg2) {
-            // TODO Auto-generated method stub
             System.out.println("After Find By"+arg1);
         }
 
         public void afterNavigateBack(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("After Navigate Back");
         }
 
         public void afterNavigateForward(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("After Navigate Forward");
         }
 
         public void afterNavigateRefresh(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("On Navigating Refresh");
         }
 
         public void afterNavigateTo(String arg0, WebDriver arg1) {
-            // TODO Auto-generated method stub
             System.out.println("On Navigating To"+arg0);
         }
 
         public void afterScript(String arg0, WebDriver arg1) {
-            // TODO Auto-generated method stub
             System.out.println("After Script");
         }
 
@@ -72,53 +62,43 @@ public class TestUtil {
         }
 
         public void beforeAlertDismiss(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("Before Alert Dismiss");
         }
 
         public void beforeChangeValueOf(WebElement arg0, WebDriver arg1,
                                         CharSequence[] arg2) {
-            // TODO Auto-generated method stub
             System.out.println("Before Change Value of"+arg0);
         }
 
         public void beforeClickOn(WebElement arg0, WebDriver arg1) {
-            // TODO Auto-generated method stub
             System.out.println("Before Click on"+arg0);
         }
 
         public void beforeFindBy(By arg0, WebElement arg1, WebDriver arg2) {
-            // TODO Auto-generated method stub
             System.out.println("Before Find By"+arg0);
         }
 
         public void beforeNavigateBack(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("Before Navigate Back");
         }
 
         public void beforeNavigateForward(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("Before Navigate Forward");
         }
 
         public void beforeNavigateRefresh(WebDriver arg0) {
-            // TODO Auto-generated method stub
             System.out.println("Before Navigate Refresh");
         }
 
         public void beforeNavigateTo(String arg0, WebDriver arg1) {
-            // TODO Auto-generated method stub
             System.out.println("Before Navigate To"+arg0);
         }
 
         public void beforeScript(String arg0, WebDriver arg1) {
-            // TODO Auto-generated method stub
             System.out.println("Before Script");
         }
 
         public void onException(Throwable arg0, WebDriver arg1) {
-            // TODO Auto-generated method stub
             System.out.println("On Exception"+arg0);
         }
 
